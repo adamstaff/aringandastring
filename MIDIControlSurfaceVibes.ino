0746190a2a34b66e5c0892952d539847343ae30d
spaffinge <52344911+spaffinge@users.noreply.github.com>
2023-09-11 17:54:21 +0100

    Update MIDIControlSurfaceVibes.ino
    pro micro died while uploading this version
    Great stuff

--- MIDIControlSurfaceVibes.ino
+++ MIDIControlSurfaceVibes.ino
@@ -30,50 +30,82 @@ int analogPin = A3;
 int pitchCenter = 512;
 int pitchTop = 1023;
 int pitchCalc = 0;
+int counter = 0;
 //button
 const int buttonPin = 2;
-int calibrating = false;
+bool calibrating = false;
+bool buttonDown = false;
 //status LED
-int LEDstate = false;
+bool LEDstate = false;
+int RXLED = 17;
+int TXLED = 30;
 
 //mapping ring position to calibration
 //and sending MIDI pitch bend changes
 void calculateAndSendPitch() {
-  // get the value from the pin,
-  // map from the center, and scale to the top
-  pitchCalc = pitchCenter - analogRead(A0) / pitchTop;
-  //send pitch bend value to channel 1
-  topLevelMidiObject.sendPitchBend(CHANNEL_1, pitchCalc);
+  if (calibrating == false) {
+    // get the value from the pin,
+    // map from the center, and scale to the top
+    int pitchBottom = pitchCenter - (pitchCenter - pitchTop);
+    pitchCalc = 16383 * ((analogRead(A0) - pitchBottom) / (pitchTop - pitchBottom));
+    //send pitch bend value to channel 1
+    topLevelMidiObject.sendPitchBend(CHANNEL_1, pitchCalc);
+  };
 };
 
 //calibrating pitch, first press sets the middle C,
 //second press sets the top note (assumed symmetrical)
 void calibratePitch() {
   //set top (second button press)
-  if (calibrating && digitalRead(buttonPin) == HIGH) {
+  if (calibrating && digitalRead(buttonPin) == HIGH && buttonDown == false) {
+    Serial.print("Got button press 2: calibrated!");
+    Serial.println();
     pitchTop = analogRead(A0);
-    calibrating = false;
     digitalWrite(LED_BUILTIN, LOW);
+    calibrating = false;
+    buttonDown = true;
+    Serial.print("(finished calibration setup, setting pitchCenter to:");
+    Serial.println();
+    Serial.print(pitchTop);
+    Serial.println();
     return;
   };
   //wait for button press, if so get value
-  if (digitalRead(buttonPin) == HIGH) {
+  if (calibrating == false && digitalRead(buttonPin) == HIGH && buttonDown == false) {
+    Serial.print("Got button press #1: starting calibration");
+    Serial.println();
     pitchCenter = analogRead(A0);
-    calibrating = true;
     digitalWrite(LED_BUILTIN, HIGH);
+    calibrating = true;
+    buttonDown = true;
+    Serial.print("(finished calibration setup, setting pitchCenter to:");
+    Serial.println();
+    Serial.print(pitchCenter);
+    Serial.println();
   };
 };
 
 //if we're in calibration mode, flash the LED
 void flashIfCalibrating() {
   if (calibrating) {
-    if (LEDstate) {
-      digitalWrite(LED_BUILTIN, HIGH);
-      LEDstate = true;
+    if (counter > 2000) {
+      counter = 0;
+      if (LEDstate == false) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        LEDstate = true;
+      } else {
+        digitalWrite(LED_BUILTIN, LOW);
+        LEDstate = false;
+      };
     } else {
-      digitalWrite(LED_BUILTIN, LOW);
-      LEDstate = false;
-    }
+      counter = counter + 1;
+    };
+  };
+};
+
+void checkButton() {
+  if (digitalRead(buttonPin) == LOW && buttonDown == true) {
+    buttonDown = false;
   };
 };
 
@@ -84,9 +116,12 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW); //turn off LED
   RelativeCCSender::setMode(REAPER_RELATIVE_1); //https://tttapa.github.io/Control-Surface-doc/Doxygen/d2/d1f/RotaryEncoder_8ino-example.html
   topLevelMidiObject.begin(); //instatiate MIDI
+  Serial.begin(9600);
 };
 
 void loop() {
+  checkButton();
+  calibratePitch();
   flashIfCalibrating(); //flashes onboard LED if we're calibrating
   calculateAndSendPitch(); //pitch bend
   Control_Surface.loop(); //other MIDI behaviour
